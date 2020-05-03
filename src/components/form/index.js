6ffc545b4b8d188c3bf63de666525bd6434371c6
geraldjän <solidcaius@gmail.com>
2020-05-03 11:42:02 +0200

    fixed setInterval

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -19,8 +19,8 @@ class Form extends React.Component{
 
         this.handleEnter = this.handleEnter.bind(this);
         this.handleShow = this.handleShow.bind(this);
-        this.function1 = this.function1.bind(this);
-        this.function2 = this.function2.bind(this);
+        this.increaseCount = this.increaseCount.bind(this);
+        this.setCurrentWord = this.setCurrentWord.bind(this);
 
     }
 
@@ -30,45 +30,41 @@ class Form extends React.Component{
 
     handleEnter(event){
 
-        const {name} = event.target;
-
         let word = document.querySelector(".field").value;
         document.querySelector(".field").value = "";
-        console.log(word);
-        
         
         this.wordList.push(word);
-        console.log(this.wordList);
         this.setState({
             words: this.wordList
         })
 
    }
 
-   function1(){
+   increaseCount(){
                 this.count++;
                 if(this.count > this.state.words.length){
                     this.count = 0;
                 }    
    }
 
-   function2(){
+   setCurrentWord(){
     this.setState({
         currentWord: this.state.words[this.state.count],
         count: this.count
     })
    }
 
-    handleShow(event){
-       setInterval( function(){
-        this.function1();
-        this.function2();
+
+//    Mount
+    handleShow(){
+       setInterval(() => {
+        this.increaseCount();
+        this.setCurrentWord();
        },1000);
     }   
 
     handleSubmit(event){
         event.preventDefault();
-
         // Do stuff
     }
 
@@ -95,13 +91,10 @@ class Form extends React.Component{
            I am a {this.state.currentWord} 
         </article>
 
-          
         </article>
       );
 
     }
 }
-
-
 
 export default Form;
